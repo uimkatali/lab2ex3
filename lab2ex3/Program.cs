40beb3e73ea3f706b821c5cfc34bcd24608aa9ac
uimkatali <b8bf6b5@gmail.com>
2023-09-26 20:40:34 +0300

    Add project files.

---
+++ lab2ex3/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace lab2ex3
+{
+    class Program
+    {
+        /*        Scrieti o functie care va determina daca un numar este sau nu numar prim.
+
+        Apelati-o si afisati-i rezultatul*/
+
+        static public int EstePrim(int x)
+        {
+            if (x > 1 & x / x == 1)
+            {
+                Console.WriteLine("X este numar prim!");
+            }
+            else { Console.WriteLine("X nu este numar prim :D"); }
+
+            return x;
+        }
+        static void Main(string[] args)
+        {
+            int num = Convert.ToInt32(Console.ReadLine());
+            int numarPrim = EstePrim(num);
+             
+        }
+       
+    }
+}
